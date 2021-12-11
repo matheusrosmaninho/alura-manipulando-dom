5fb64f1ef0244ca3d562db4eeed7b44151387713
Matheus Rosmaninho <matheusrosmano@gmail.com>
2021-12-11 11:08:33 -0300

    Conclui a tarefa

---
+++ listaDeTarefas.js
@@ -0,0 +1,38 @@
+const criarTarefa = (event) => {
+  event.preventDefault();
+
+  const list = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input");
+  const valor = input.value;
+
+  const tarefa = document.createElement("li");
+  tarefa.classList.add("task");
+
+  const conteudo = `<p class="content">${valor}</p>`;
+  tarefa.innerHTML = conteudo;
+
+  tarefa.appendChild(BotaoConclui());
+  list.appendChild(tarefa);
+
+  input.value = "";
+};
+
+const novaTarefa = document.querySelector("[data-form-button");
+
+novaTarefa.addEventListener("click", criarTarefa);
+
+const BotaoConclui = () => {
+  const botaoConclui = document.createElement("button");
+  botaoConclui.classList.add("check-button");
+  botaoConclui.innerText = "concluir";
+  botaoConclui.addEventListener("click", concluirTarefa);
+  return botaoConclui;
+};
+
+const concluirTarefa = (event) => {
+  const botaoConclui = event.target;
+
+  const tarefaCompleta = botaoConclui.parentElement;
+
+  tarefaCompleta.classList.toggle("done");
+};
